5f31ee0116065d76d80404b4291119911cfac3ee
BawanthaKonara <bawanthakonara@gmail.com>
2018-06-03 15:17:26 +0530

    this is comment for commit

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld
+{
+    public void Helloworld()
+    {
+        System.out.println("Hello World");
+    }
+
+    
+    public static void main(String[] args)
+    {
+        HelloWorld obj = new HelloWorld();
+        obj.Helloworld();
+    }
+    
+}
